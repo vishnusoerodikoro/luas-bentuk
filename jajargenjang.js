ac83fc282ad1d6fcf45ac8f4eda261f16a0d9ad4
vishnusoerodikoro <44632142+vishnusoerodikoro@users.noreply.github.com>
2022-05-30 19:56:27 +0700

    Update jajargenjang.js

--- jajargenjang.js
+++ jajargenjang.js
@@ -1,1 +1,16 @@
-halo test
+class luasbentuk {
+    constructor(alas, tinggi) {
+      this.alas = alas;
+      this.tinggi = tinggi;
+    }
+   
+    hitungluasbentuk() {
+return (this.alas * this.tinggi);
+    }
+}
+    jajargenjang = new luasbentuk('10','5');
+
+
+//console.log(hitungluasbentuk)
+
+jajargenjang.hitungluasbentuk(5,5);
