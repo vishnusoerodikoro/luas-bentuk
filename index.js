0501f228831276bd02fa1d103f942ab6eee980c9
lintang <lintangmochz@gmail.com>
2022-05-31 15:12:04 +0700

    perubahan dan penambahan code, final

--- index.js
+++ index.js
@@ -1,43 +1,53 @@
 // 1. Menghitung Luas Jajar Genjang
-class luasbentuk {
-    constructor(alas, tinggi, lebar, panjang, jari) {
+class Luasbentuk {
+    constructor(alas, tinggi, jari, panjang, lebar, sisi) {
       this.alas = alas;
       this.tinggi = tinggi;
-      this.lebar = lebar;
-      this.panjang = panjang;
       this.jari = jari;
+      this.panjang = panjang;
+      this.lebar = lebar;
+      this.sisi = sisi;
     }
    
     rumusJajargenjang() {
     return this.alas * this.tinggi
     }
 
-    hitungJajargenjang() {
-        console.log(`1. Menghitung Luas Jajar Genjang. Jika diinputkan Alas : ` + this.alas + 
-        ` ,dan Tinggi : ` + this.tinggi + `.\n   ,maka Luas Jajaran Genjangnya adalah = ` , this.rumusJajargenjang())
-    }
-
     rumusSegitiga() {
         return this.alas * this.tinggi / 2
     }
 
-    hitungSegitiga() {
-        console.log(`2. Menghitung Luas Segitiga. Jika diinputkan Alas : ` + this.alas + 
-        ` ,dan Tinggi : ` + this.tinggi + `.\n   ,maka Luas Segitiganya adalah = ` , this.rumusSegitiga())
+    rumusLingkaran() {
+        const phi = 3.14
+        return this.jari**2 * phi
+    }
+
+    rumusPersegipanjang() {
+        return this.panjang * this.lebar
+    }
+
+    rumusPersegi() {
+        return this.sisi**2
     }
 }
 
-jajargenjang = new luasbentuk(10, 5)
-segitiga = new luasbentuk(3, 5)
+jjg = new Luasbentuk(10, 5)
+segitiga = new Luasbentuk(3, 5)
+lkg = new Luasbentuk(0, 0, 2)
+psgp = new Luasbentuk(0, 0, 0, 4, 8)
+psg = new Luasbentuk(0, 0, 0, 0, 0, 5)
 
-jajargenjang.hitungJajargenjang()
-segitiga.hitungSegitiga()
+console.log( `1. Menghitung Luas Jajar Genjang. Jika diinputkan Alas = 10, 
+dan Tinggi = 5, maka Luas Jajaran Genjangnya adalah = ${jjg.rumusJajargenjang()}`)
 
-// 2. Menghitung Luas Lingkaran
-const phi = 3.14
-let jari = 2
-let lL = (jari) => {
-    return phi * (jari*jari);
-}
-console.log('3. Menghitung Luas Lingkarang. Jika Jari-jarinya diinput : ' + jari + 
-            '.\n   ,maka Luas Lingkarannya adalah = ' + lL(jari));
+console.log(`2. Menghitung Luas Segitiga. Jika diinputkan Alas = 3,
+dan Tinggi = 5, maka Luas Segitiganya adalah = ${segitiga.rumusSegitiga()}`)
+
+console.log(`3. Menghitung Luas Lingkaran. Jika Jari-jarinya diinput = 2,
+maka Luas Lingkarannya adalah = ${lkg.rumusLingkaran()}`)
+
+console.log(`4. Menghitung Luas Persegi Panjang. Jika panjangnya diinput = 4,
+dan Lebar = 8, maka Luas Peregi Panjang adalah = ${psgp.rumusPersegipanjang()}`)
+
+console.log(`5. Menghitung Luas Persegi Panjang. Jika sisinya diinput = 5,
+maka Luas Peregi Panjang adalah = ${psg.rumusPersegi()}`)
