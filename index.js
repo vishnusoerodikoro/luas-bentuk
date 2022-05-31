2e960d0f2d1b5f398adf280f05759a12819bb7c2
unknown <fanri777@gmail.com>
2022-05-31 10:44:45 +0700

    Perubahan & ngerapihin function Jajar Genjang dan Lingkaran

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// 1. Menghitung Luas Jajar Genjang
 class luasbentuk {
     constructor(alas, tinggi) {
       this.alas = alas;
@@ -5,11 +6,12 @@ class luasbentuk {
     }
    
     hitungluasbentuk() {
-return (this.alas * this.tinggi);
+    return (this.alas * this.tinggi);
     }
 
     hitung() {
-        console.log(`Luas Jajar Genjang` , this.hitungluasbentuk());
+        console.log(`1. Menghitung Luas Jajar Genjang. Jika diinputkan Alas : ` + this.alas + 
+        ` ,dan Tinggi : ` + this.tinggi + `.\n   ,maka Luas Jajaran Genjangnya adalah = ` , this.hitungluasbentuk());
     }
 }
     jajargenjang = new luasbentuk('10','5');
@@ -19,5 +21,13 @@ return (this.alas * this.tinggi);
 
 jajargenjang.hitung();
 
-console.log(jajargenjang.hitungluasbentuk());
 
+
+// 2. Menghitung Luas Lingkaran
+const phi = 3.14
+let jari = 2
+let lL = (jari) => {
+    return phi * (jari*jari);
+}
+console.log('2. Menghitung Luas Lingkarang. Jika Jari-jarinya diinput : ' + jari + 
+            '.\n   ,maka Luas Lingkarannya adalah = ' + lL(jari));
